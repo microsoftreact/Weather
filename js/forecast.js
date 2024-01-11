e84252da94afe394600853c5da8e6440675813b2
nonoteacher <nodirbektoshpulatov123345@gmail.com>
2024-01-12 00:01:37 +0500

    finish API project

---
+++ js/forecast.js
@@ -0,0 +1,16 @@
+const KEY = "96b947a45d33d7dc1c49af3203966408";
+
+// request get data
+
+const getData = async (city) => {
+  const base = "https://api.openweathermap.org/data/2.5/weather";
+  const query = `?q=${city}&units=metric&appid=${KEY}`;
+  loader(true);
+  const req = await fetch(base + query);
+  const data = await req.json();
+  loader(false);
+
+  return data;
+};
+
+// getData("London").then((data) => console.log(data));
